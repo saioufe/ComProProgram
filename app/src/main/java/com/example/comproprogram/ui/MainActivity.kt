04f56555b09f10e87493c5252735906fa3c8608e
saioufe <saioufeyoutube@yahoo.com>
2022-11-03 23:12:41 -0500

    data binding for MainActivity

--- app/src/main/java/com/example/comproprogram/ui/MainActivity.kt
+++ app/src/main/java/com/example/comproprogram/ui/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.mainContainer, studentFragment).commit()
         }
+
         button3.setOnClickListener { view: View ->
             shape3.visibility = View.VISIBLE
             visiableShape.set(homeBinding.thirdShape)
